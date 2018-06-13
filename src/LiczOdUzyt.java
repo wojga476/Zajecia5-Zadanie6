ebf3083fc8540e6efe023232b18278c90c80f202
wojga476 <wojga476@gmail.com>
2018-06-13 21:31:06 +0200

    poprawa druga-wyświetlenie komunikatów o liczbach użytkownika

--- src/LiczOdUzyt.java
+++ src/LiczOdUzyt.java
@@ -7,17 +7,18 @@ public class LiczOdUzyt {
         System.out.println("Podaj liczbę:");
         int wynik = input.nextInt();
         
-        while (wynik < 100 || wynik > 200) {
+        while (wynik < 100 || wynik > 200 || wynik%3 !=0) {
             
-            System.out.println("Liczba musi być z przedziału (100,200).Podaj liczbę:");
+            System.out.println("Liczba musi być z przedziału (100,200) i być podzielna przez 3.Podaj liczbę:");
             wynik = input.nextInt();
             
         }
-            if (wynik % 3 == 0) {
-                System.out.println("Liczba jest podzielna przez 3");
-            } else if (wynik % 3 != 0) {
-                System.out.println("Liczba jest nie podzielna przez 3");
-            }
+            System.out.println("Twoja liczba jest ok");
+//            if (wynik % 3 == 0) {
+//                System.out.println("Liczba jest podzielna przez 3");
+//            } else if (wynik % 3 != 0) {
+//                System.out.println("Liczba jest nie podzielna przez 3");
+//            }
         
         input.close();
     }
